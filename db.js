e11baea51bde5a2c118869c3fe8532f4b0cd0341
Joana <joir.bilbao@alumnos.cei.es>
2024-05-03 17:00:51 +0200

    finalizacion del front

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
+// Carga la configuración de variables de entorno
 require("dotenv").config();
 const {MongoClient, ObjectId} = require("mongodb");
 
+//Retorna una promesa que se resuelve cuando se establece la conexión con la base de datos MongoDB
 function conectar(){
     return MongoClient.connect(process.env.URL_DB);
 }
@@ -9,7 +11,10 @@ function getTareas(){
     return new Promise(async (ok,ko) => {
         try{
             const conexion = await conectar();
-            let coleccion = conexion.db("tareas").collection("tareas");       
+
+            // Obtiene la colección de tareas
+            let coleccion = conexion.db("tareas").collection("tareas");
+            // Busca todas las tareas en la colección y las convierte en un array       
             let tareas = await coleccion.find({}).toArray();
             conexion.close();
             ok(tareas);
@@ -24,7 +29,9 @@ function crearTarea(tarea){
     return new Promise(async (ok,ko) => {
         try{
             const conexion = await conectar();
-            let coleccion = conexion.db("tareas").collection("tareas");       
+            let coleccion = conexion.db("tareas").collection("tareas"); 
+          
+            // Inserta la tarea en la colección y obtiene el ID de la tarea insertada
             let {insertedId} = await coleccion.insertOne(tarea);
             conexion.close();
             ok(insertedId);
@@ -39,7 +46,9 @@ function borrarTarea(id){
     return new Promise(async (ok,ko) => {
         try{
             const conexion = await conectar();
-            let coleccion = conexion.db("tareas").collection("tareas");       
+            let coleccion = conexion.db("tareas").collection("tareas");
+            
+            // Borra la tarea con el ID de la colección y obtiene la cantidad de documentos borrados (0-1)
             let {deletedCount} = await coleccion.deleteOne({_id: new ObjectId(id)});
             conexion.close();
             ok(deletedCount);
@@ -50,4 +59,57 @@ function borrarTarea(id){
     } );
 }
 
-module.exports = {getTareas, crearTarea, borrarTarea};
+function actualizarEstado(id){
+
+    return new Promise(async (ok,ko) => {
+        
+        try{
+            const conexion = await conectar();
+            let coleccion = conexion.db("tareas").collection("tareas");
+            let tarea = await coleccion.findOne({_id: new ObjectId(id)});
+
+            // Si la tarea no existe, se cierra la conexión y se rechaza creando un error
+            if(!tarea){
+                conexion.close();
+                return ko({error : "la tarea no existe"});
+            }
+
+            // Cambia el estado de la tarea y lo actualiza en la colección    
+            let estado = !tarea.terminada;
+
+            let {modifiedCount} = await coleccion.updateOne({_id: new ObjectId(id)}, {$set: {terminada: estado}});
+            conexion.close();
+            ok(modifiedCount);
+
+        }catch(error){
+            ko({error : "error en la base de datos"});
+        }
+
+    });
+
+}
+
+function actualizarTexto(id,tarea){
+
+    return new Promise(async (ok,ko) => {
+
+        try{
+        
+            const conexion = await conectar();
+            let coleccion = conexion.db("tareas").collection("tareas");
+
+            // Actualiza el texto del documento con el ID especifico
+            let {modifiedCount} = await coleccion.updateOne({_id: new ObjectId(id)}, {$set: {tarea}});
+            conexion.close();
+            ok(modifiedCount);
+
+        }catch(error){
+            ko({error : "error en la base de datos"});
+        }
+
+    });
+
+}
+
+
+module.exports = {getTareas, crearTarea, borrarTarea, actualizarEstado, actualizarTexto};
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const {json} = require("body-parser");
 const cors = require("cors");
-const {getTareas, crearTarea, borrarTarea} = require("./db");
+const {getTareas, crearTarea, borrarTarea, actualizarEstado, actualizarTexto } = require("./db");
 
+//instancia de Express
 const servidor = express();
 
 servidor.use(cors());
@@ -16,7 +17,8 @@ servidor.get("/tareas", async (peticion, respuesta) => {
     try{
         let tareas = await getTareas();
 
-        tareas = tareas.map( ({_id, tarea, estado}) => { return {id: _id, tarea, estado} } );
+        // Mapea las tareas para devolver (_id, tarea, terminada) y crea una propiedad id y le asigna el valor de la variable _id
+        tareas = tareas.map( ({_id, tarea, terminada}) => { return {id: _id, tarea, terminada} } );
 
         respuesta.json(tareas);
 
@@ -45,10 +47,43 @@ servidor.post("/tareas/nueva", async (peticion, respuesta, siguiente) => {
     
 });
 
-servidor.delete("/tareas/borrar/:id([a-f0-9]{24})", async (peticion, respuesta) => {
+servidor.delete("/tareas/borrar/:id([0-9a-fA-F]{24})", async (peticion, respuesta) => {
     try{
         let cantidad = await borrarTarea(peticion.params.id);
         respuesta.json({ resultado : cantidad > 0 ? "ok" : "ko" });
+
+    }catch(error){
+        
+        respuesta.status(500);
+        respuesta.json(error);
+    }
+});
+
+servidor.put("/tareas/actualizar/:id([0-9a-fA-F]{24})/:operacion(1|2)", async (peticion, respuesta) => {
+    // Obtiene el número de la solicitud y lo convierte a número
+    let operacion = Number(peticion.params.operacion);
+
+    // Array de funciones de operaciones a realizar [0,1]
+    let operaciones = [actualizarTexto,actualizarEstado];
+
+    let {tarea} = peticion.body;
+
+    // Verifica si la operación es para actualizar el texto y, filtrar si no hay tarea comprobar si esta vacia
+    if(operacion == 1 && (!tarea || tarea.trim() == "")){
+        return siguiente({ error : "falta el argumento tarea en el objeto JSON" });
+    }
+    
+    try{
+        // Ejecuta la operación correspondiente (0 o 1) en la base de datos
+        // Obtiene el valor del parámetro id de la URL de la solicitud y, 
+        // verifica el valor de la variable operacion, 
+        // si la operación es igual a 0, actualiza texto de la tarea. Si la operación es 1 actualiza el estado
+        let cantidad = await operaciones[operacion - 1](peticion.params.id, operacion == 1 ? tarea : null);
+
+        // Imprime la cantidad de tareas actualizadas en la consola 0-1
+        console.log(cantidad);
+
+        respuesta.json({ resultado : cantidad ? "ok" : "ko "});
 
     }catch(error){
         respuesta.status(500);
@@ -68,5 +103,5 @@ servidor.use((error, peticion, respuesta, siguiente) => {
     respuesta.json({ error : "petición no válida" });
 });
 
-
+//Inicia el servidor y lo hace escuchar en el puerto especificado en la variable de entorno 'PORT'
 servidor.listen(process.env.PORT);
